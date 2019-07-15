3abbca366c94be03faec469151114d670102036b
RTestai <raphaeltestai.eng@gmail.com>
2019-07-15 16:33:27 -0300

    Segundo commit - adicionando o script e modificando o README.txt

---
+++ R/trabalho_RaphaelTestai.R
@@ -0,0 +1,13 @@
+# Para listar os Objetos no Workspace
+ls()
+
+#Para remover
+rm(lista = ls())
+
+# Qual é a pasta de trabalho
+getwd()
+
+a <- c(5,4,3,2,1)
+
+a
+
---
+++ README.txt
@@ -0,0 +1,1 @@
+#Este é o repositório da disciplina ENBT 2019 ministrado por Andrea Sanchez-Tapia, Sara Mortara, Diogo Rocha.
