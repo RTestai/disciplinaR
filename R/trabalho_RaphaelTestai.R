ad11b5cf61849a6b2d11fcbf167a129b128d3535
RTestai <raphaeltestai.eng@gmail.com>
2019-07-16 15:40:31 -0300

    atualização 15:40

--- R/trabalho_RaphaelTestai.R
+++ R/trabalho_RaphaelTestai.R
@@ -40,13 +40,16 @@ library(raster)
 #list.dirs() lista todos os diretórios
 # o ponto "." resume todo caminho de antes. Dois pontos ".." é o caminho anterior.
 
-# Principais classes do R:
-#   vetor: sequência de valores numéricos ou de caracteres.Um ou outro.
-#   matriz:Sequência de linhas e colunas.são vários vetoroes com o mesmo comprimento. Ou um ou outro.
-#   data frame: como uma matriz, mas aceita vários tipos.
-#   lista: conjunto de vetores, dataframes ou matrizes.
-#   função: São objetos do R, r o seu conteúdo pode ser visto digitando o nome da função.
-#   outros: rasterlayer, rasterstack...
+
+
+### Principais classes do R:
+
+  #   vetor: sequência de valores numéricos ou de caracteres.Um ou outro.
+  #   matriz:Sequência de linhas e colunas.são vários vetoroes com o mesmo comprimento. Ou um ou outro.
+  #   data frame: como uma matriz, mas aceita vários tipos.
+  #   lista: conjunto de vetores, dataframes ou matrizes.
+  #   função: São objetos do R, r o seu conteúdo pode ser visto digitando o nome da função.
+  #   outros: rasterlayer, rasterstack...
 
 
 ### Vetores: 
@@ -59,15 +62,47 @@ library(raster)
   # A função class() retorna o tipo do objeto.
 
 ## Exemplo de factor:
+
   # x <- c(macho, femea, macho, femea, macho, macho)
   # y <- factor(x)
   # y
   # Vai retornar o que está dentro de x e os níveis.
 
 ## Indexação:
+
   # x[1], isso vai retornar o valor na posição 1.
   # x(c(2,4)) vai retornar o conjunto de posições 2 e 4 para o vetor x. 
   # x < 4, assim ele retornará um vetor lógio afirmando quem é menor ou não (true ou false).
   # Os dois colchetes fazem referência, geralmente, às listas, para pegar o objeto dentro dela. 
-alterar o valor do elemento do vetor x[2] <- 5.
-x[x<2] <- 0 vai substituir por zero todos os valores menores que 2.
+  # alterar o valor do elemento do vetor x[2] <- 5.
+  # x[x<2] <- 0 vai substituir por zero todos os valores menores que 2.
+  # lenght()
+  # Head()
+
+### Matriz
+
+  # quando coloca [,1] significa pegar todas as linhas da primeira coluna, e o oporto é o mesmo.
+  # criar matriz: matrix(1:9, nrow = 3, ncol = 3)
+  # matriz: matrix(1:9, nrow = 3, ncol = 3, byrow = true), ele faz preenchendo pela linha.
+  # cbind() irá juntar as colunas. Exemplo: ring <- c(1,2,3,4,5,6,7), ring2 = c(2.3,6,7,8,9,8,4)
+  # mat <- cbind(ring,ring2)
+  # dim(mat) retorna o tamanho da matriz, em linhas e colunas.
+  # colnames() nome das colunas. A mesma coisa para as linhas.
+  # rownames <- c(a1,a2,a3,a4,a5,a6,a7) vai renomear as linhas. O mesmo para as colunas.
+  # Para indexar baste apenas utilizar uma vírgula para separar a posição linha da coluna.
+  # Para modificar a matriz é a mesma coisa do vetor. 
+
+### Data Frame
+
+  # Aceita mais de um tipo de objeto, podendo ser integer, caracter, numeric, factor
+  # a <- LETTER[1:5]
+  # fac <- sample(a,10, replace = true) vai reamostrar com reposição.
+  # x <- data.frame(b = 1, c = 1:10,fac)
+  # str() mostra a estrutura do objeto.
+  # Para que as strings repetidas não mudem para factor, deve-se usar o stringsAsFactor() = FALSE
+  # head() mostra os 6 primeiros, mas pode-se incluir um argumento para visualizar uma quantidade maior.
+  # pode-se usar cbind(x,$state...), state é o nome da nova coluna.
+
+
+
+
