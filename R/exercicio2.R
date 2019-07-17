e88c0e801ef3723480dff718ad3f3334bebdfc57
RTestai <raphaeltestai.eng@gmail.com>
2019-07-17 10:47:21 -0300

    atualizando

--- R/exercicio2.R
+++ R/exercicio2.R
@@ -1,7 +1,12 @@
-tabela <- read.csv("./data/exercicio2_Corrigido.csv",sep = ";";header=TRUE)
+tabela <- read.csv("./data/teste.csv",sep = ";",header=TRUE)
 
-tabela
-y <- mean(x = tabela$uptake)
+#p <- tabela[tabela$Localidade == "Quebec" & tabela$Tipo == "chilled","uptake"]
+#p
+
+x <- subset(tabela, Localidade == "Quebec" & Tipo == "chilled")
+y <- mean(x$uptake)
 
 y
+
+
 
